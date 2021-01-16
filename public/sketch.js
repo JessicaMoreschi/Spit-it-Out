af4c252b1bf31f24308cc66fa73aa5960ecef9b1
JessicaMoreschi <jessica.moreschi98@gmail.com>
2021-01-16 17:36:10 +0100

    setup

--- public/sketch.js
+++ public/sketch.js
@@ -1,52 +1,135 @@
-// Create a new connection using socket.io (imported in index.html)
-let socket = io();
+/**
+ * noise values (noise 2d) are used to animate a bunch of agents.
+ *
+ * KEYS
+ * space               : new noise seed
+ * backspace           : clear screen
+ * s                   : save png
+ *
+ * to do
+ *
+ */
 
-// define the function that will be called on a new newConnection
-socket.on("connect", newConnection);
 
-function newConnection() {
-  console.log("your id:", socket.id);
-}
-//PROVA
+var x = 0;
+var y = 0;
+var stepSize = 0.01;
 
-// Define which function should be called when a new message
-// comes from the server with type "mouseBroadcast"
+var font = 'Georgia';
+var letters = 'ciao ';
+//'Così tra questa immensità s\'annega il pensier mio: e il naufragar m\'è dolce in questo mare.'
+var fontSizeMin = 14;
 
-socket.on("mouseBroadcast", otherMouse);
+//impostazioni riconoscimento vocale //
+let lang = 'en-US'; //|| 'it-IT'
+let speechRec = new p5.SpeechRec(lang, gotSpeech);
+let vol_map;
+let vol2 =1;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background("purple");
-}
+var sketch = function(p) {
+  var agents = [];
+  var init = 0;
+  var agentCount = 0; // initial agents
+  var maxAgentCount = 10; // max agents
+  var noiseScale = 500; // you can modify it to change the vorticity of the flux
+  var noiseStrength = 10;
+  var overlayAlpha = 0.04;
+  //var agentAlpha = 10;
+  var strokeWidth = 0.3;
 
-// Callback function called when a new message comes from the server
-// Data parameters will contain the received data
-function otherMouse(data) {
-  console.log("received:", data);
-  noStroke();
-  fill("yellow");
-  ellipse(data.x, data.y, 20);
-}
 
-function mouseDragged() {
-  console.log("sending: ", mouseX, mouseY);
-  noStroke();
-  fill(255);
+  p.setup = function() {
+    p.createCanvas(p.windowWidth, p.windowHeight);
 
-  // create an object containing the mouse position
-  let message = {
-    x: mouseX,
-    y: mouseY,
+    mic = new p5.AudioIn();
+    mic.start();
+
+    b1 = p.createButton('microfono');
+    b1.position(p.width / 2 * 1.7, p.height / 2 * 0.1);
+    b1.mousePressed(listener);
+    b1.id('startBtn');
+
+    p.colorMode(p.HSB, 360, 100, 100); //colorMode(mode, max1, max2, max3, [maxA])
+    p.textFont(font, fontSizeMin);
+
+//togliere le seguenti tre righe se si vuole inserire tutti gli agents cliccando
+    for (var i = 0; i < agentCount; i++) { //così ci sono già di default #agentCount agents
+      agents[i] = new Agent(p.random(p.width), p.random(p.height), p.color(p.random(360), 80, 60), letters, vol_map);
+    }
+
   };
-  // send the object to server,
-  // tag it as "mouse" event
-  socket.emit("mouse", message);
 
-  ellipse(mouseX, mouseY, 20);
+  p.draw = function() {
+    p.frameRate(9); // questo per far brutalmente rallentare le scritte
+    // //volume
+    vol = p.round(mic.getLevel(), 2);
+    vol_map = p.map(vol, 0, 1, 1,150);
+    console.log("volume " + vol_map);
+
+    if ( p.getAudioContext().state !== 'running') {
+          p.text('non funziona audio', p.width/2, p.height/2);
+      } else {
+        p.text('audio abilitato',  p.width/2, p.height/2);
+      }
+
+
+    p.fill(255, overlayAlpha);
+    p.noStroke();
+    p.rect(0, 0, p.width, p.height);
+
+    // Draw agents
+    if (agentCount > maxAgentCount){
+      init = agentCount - maxAgentCount;
+    }
+    for (var i = init; i < agentCount; i++) {
+      agents[i].update(noiseScale, noiseStrength, strokeWidth);
+    }
+  } //fine draw;
+
+  p.keyReleased = function() {
+    if (p.key == 's' || p.key == 'S') p.saveCanvas(gd.timestamp(), 'png');
+    if (p.key == ' ') {
+      var newNoiseSeed = p.floor(p.random(10000));
+      p.noiseSeed(newNoiseSeed);
+    }
+    if (p.keyCode == p.DELETE || p.keyCode == p.BACKSPACE) p.background(255);
+  };
+
+  p.mouseClicked = function(){
+    agentCount++;
+    //console.log(agentCount + " agents");
+    agents[agentCount-1] = new Agent(p.mouseX, p.mouseY, p.color(p.random(360), 80, 60), letters, vol_map);
+    if (p.getAudioContext().state !== 'running') {
+      p.getAudioContext().resume();
+    }
+  }
+
+  p.windowResized = function() {
+    p.resizeCanvas(p.windowWidth, p.windowHeight);
+  }
+
+listener = function(){
+  let continuous = true; //continua a registrare
+  let interim = false;
+  speechRec.start(continuous, interim);
 }
 
-function draw() {
-  // evert draw cycle, add a background with low opacity
-  // to create the "fade" effect
-  background(0, 5);
+}; //fine sketch
+
+var myp5 = new p5(sketch);
+
+function gotSpeech() {
+  if (speechRec.resultValue) {
+     let text = speechRec.resultString;
+     letters = text + ' ';
+     console.log(speechRec.resultString)
+  }
 }
+
+function micGif(){
+document.getElementById('micBtn').style.backgroundImage="url('../assets/image/04.2_Mic.gif')";
+// myp5.listener()
+}
+
+function micPng(){
+document.getElementById('micBtn').style.backgroundImage="url('../assets/image/04.1_Mic fermo.png')"}
