060dd788e46e55c4d0312337dd3297f5625e3563
JessicaMoreschi <jessica.moreschi98@gmail.com>
2021-01-18 11:10:50 +0100

    add firebase

--- public/sketch.js
+++ public/sketch.js
@@ -24,7 +24,7 @@ var fontSizeMin = 14;
 let lang = 'en-US'; //|| 'it-IT'
 let speechRec = new p5.SpeechRec(lang, gotSpeech);
 let vol_map;
-let vol2 =1;
+let vol2 = 1;
 
 var sketch = function(p) {
   var agents = [];
@@ -39,6 +39,29 @@ var sketch = function(p) {
 
 
   p.setup = function() {
+    //FIREBASE SETTINGS
+    var database = firebase.database(); //start database
+    var texts = database.ref('texts'); //start collection
+    //write data
+    var data = { //crate user data
+      feelColour: 'rgb(0,0,255)',
+      sentence: 'example_sentence',
+    }
+    texts.push(data); //push user data to the firebase collection
+    //read data
+    texts.on("value", gotData); //The “value” event is triggered when changes are made to the database
+    var readData = []; //read data container
+    function gotData(data) {
+      var texts = data.val(); //The val() function returns an object.
+      var keys = Object.keys(texts); // Grab the keys to iterate over the object
+      for (var i = keys.length - 1; i < keys.length; i++) { //select last object
+        var userText = texts[keys[i]]; //assign his data to var userText
+        readData.push(userText) //push user data to the readData container
+      }
+      console.log(readData);
+    }
+    //END FIREBASE SETTINGS
+
     p.createCanvas(p.windowWidth, p.windowHeight);
 
     mic = new p5.AudioIn();
@@ -52,7 +75,7 @@ var sketch = function(p) {
     p.colorMode(p.HSB, 360, 100, 100); //colorMode(mode, max1, max2, max3, [maxA])
     p.textFont(font, fontSizeMin);
 
-//togliere le seguenti tre righe se si vuole inserire tutti gli agents cliccando
+    //togliere le seguenti tre righe se si vuole inserire tutti gli agents cliccando
     for (var i = 0; i < agentCount; i++) { //così ci sono già di default #agentCount agents
       agents[i] = new Agent(p.random(p.width), p.random(p.height), p.color(p.random(360), 80, 60), letters, vol_map);
     }
@@ -63,14 +86,14 @@ var sketch = function(p) {
     p.frameRate(9); // questo per far brutalmente rallentare le scritte
     // //volume
     vol = p.round(mic.getLevel(), 2);
-    vol_map = p.map(vol, 0, 1, 1,150);
+    vol_map = p.map(vol, 0, 1, 1, 150);
     console.log("volume " + vol_map);
 
-    if ( p.getAudioContext().state !== 'running') {
-          p.text('non funziona audio', p.width/2, p.height/2);
-      } else {
-        p.text('audio abilitato',  p.width/2, p.height/2);
-      }
+    if (p.getAudioContext().state !== 'running') {
+      p.text('non funziona audio', p.width / 2, p.height / 2);
+    } else {
+      p.text('audio abilitato', p.width / 2, p.height / 2);
+    }
 
 
     p.fill(255, overlayAlpha);
@@ -78,7 +101,7 @@ var sketch = function(p) {
     p.rect(0, 0, p.width, p.height);
 
     // Draw agents
-    if (agentCount > maxAgentCount){
+    if (agentCount > maxAgentCount) {
       init = agentCount - maxAgentCount;
     }
     for (var i = init; i < agentCount; i++) {
@@ -95,10 +118,10 @@ var sketch = function(p) {
     if (p.keyCode == p.DELETE || p.keyCode == p.BACKSPACE) p.background(255);
   };
 
-  p.mouseClicked = function(){
+  p.mouseClicked = function() {
     agentCount++;
     //console.log(agentCount + " agents");
-    agents[agentCount-1] = new Agent(p.mouseX, p.mouseY, p.color(p.random(360), 80, 60), letters, vol_map);
+    agents[agentCount - 1] = new Agent(p.mouseX, p.mouseY, p.color(p.random(360), 80, 60), letters, vol_map);
     if (p.getAudioContext().state !== 'running') {
       p.getAudioContext().resume();
     }
@@ -108,12 +131,12 @@ var sketch = function(p) {
     p.resizeCanvas(p.windowWidth, p.windowHeight);
   }
 
-listener = function(){
-  let continuous = true; //continua a registrare
-  let interim = false;
-  speechRec.start(continuous, interim);
-  console.log("listening");
-}
+  listener = function() {
+    let continuous = true; //continua a registrare
+    let interim = false;
+    speechRec.start(continuous, interim);
+    console.log("listening");
+  }
 
 }; //fine sketch
 
@@ -121,16 +144,17 @@ var myp5 = new p5(sketch);
 
 function gotSpeech() {
   if (speechRec.resultValue) {
-     let text = speechRec.resultString;
-     letters = text + ' ';
-     console.log(speechRec.resultString)
+    let text = speechRec.resultString;
+    letters = text + ' ';
+    console.log(speechRec.resultString)
   }
 }
 
-function micGif(){
-document.getElementById('micBtn').style.backgroundImage="url('../assets/image/04.2_Mic.gif')";
-myp5.listener()
+function micGif() {
+  document.getElementById('micBtn').style.backgroundImage = "url('../assets/image/04.2_Mic.gif')";
+  myp5.listener()
 }
 
-function micPng(){
-document.getElementById('micBtn').style.backgroundImage="url('../assets/image/04.1_Mic fermo.png')"}
+function micPng() {
+  document.getElementById('micBtn').style.backgroundImage = "url('../assets/image/04.1_Mic fermo.png')"
+}
